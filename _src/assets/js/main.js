b52c2197571b2ef9967d66d26675ad66a95160f2
Rosanna Blanco <rosanna.blanco.blanco@gmail.com>
2020-03-20 14:08:56 +0100

    Add function for save the properties show of data

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -3,22 +3,26 @@
 const inputElement = document.querySelector('.js-input');
 const btnElement = document.querySelector('.js-btn');
 
+//
+let dataShow = new Array();
+
 function getDataApi() {
   const inputElementValue = inputElement.value;
   fetch('http://api.tvmaze.com/search/shows?q=' + inputElementValue)
     .then(response => response.json())
     .then(data => {
-      for (let i = 0; i < data.length; i++) {
-        const dataShow = data[i].show;
-        saveResult(dataShow);
-      }
+      saveDataShow(data);
     });
   // eslint-disable-next-line no-console
 }
 
 //function save result data API
-function saveResult(pDataShow) {
-  console.log(pDataShow);
+function saveDataShow(pData) {
+  for (let i = 0; i < pData.length; i++) {
+    let data = pData[i].show;
+    dataShow.push(data);
+  }
+  /*  console.log(dataShow); */
 }
 
 btnElement.addEventListener('click', getDataApi);
