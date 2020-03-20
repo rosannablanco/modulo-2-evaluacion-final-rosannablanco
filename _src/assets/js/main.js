aad51485948c390fb5ebf8dd24a65f773da92072
Rosanna Blanco <rosanna.blanco.blanco@gmail.com>
2020-03-20 09:22:31 +0100

    save data Api

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -3,18 +3,22 @@
 const inputElement = document.querySelector('.js-input');
 const btnElement = document.querySelector('.js-btn');
 
-fetch('http://api.tvmaze.com/search/shows?q=girls')
-  .then(response => response.json())
-  .then(data => {
-    for (let i = 0; i < data.length; i++) {
-      const element = data[i].show;
-      console.log(element);
-    }
-  });
-
-function getData() {
+function getDataApi() {
+  const inputElementValue = inputElement.value;
+  fetch('http://api.tvmaze.com/search/shows?q=' + inputElementValue)
+    .then(response => response.json())
+    .then(data => {
+      for (let i = 0; i < data.length; i++) {
+        const dataShow = data[i].show;
+        saveResult(dataShow);
+      }
+    });
   // eslint-disable-next-line no-console
-  console.log(inputElement.value);
 }
 
-btnElement.addEventListener('click', getData);
+//function save result data API
+function saveResult(pDataShow) {
+  console.log(pDataShow);
+}
+
+btnElement.addEventListener('click', getDataApi);
