c82064303e9cc07c238ac8397f6de07ae7618052
Rosanna Blanco <rosanna.blanco.blanco@gmail.com>
2020-03-20 11:47:43 +0100

    Add event fav and include select in array fav

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -5,6 +5,7 @@ const btnElement = document.querySelector('.js-btn');
 const ulElement = document.querySelector('.js-ul-movie');
 //arrays results
 let dataShowResult = new Array();
+let showFavResult = new Array();
 
 function getDataApi() {
   const inputElementValue = inputElement.value;
@@ -13,24 +14,26 @@ function getDataApi() {
     .then(data => {
       for (let i = 0; i < data.length; i++) {
         dataShowResult = data[i].show;
-        saveResult();
+        saveResult(dataShowResult);
       }
     });
   /* .catch(error => console.log(`Ha sucedido un error: ${error}`)); */
 }
 
 //function save result data API
-function saveResult() {
-  paintResultShow();
+function saveResult(pDataShowResult) {
+  let resultShowSearch = pDataShowResult;
+  paintResultShow(resultShowSearch);
 }
-function paintResultShow() {
+//Paint list result search
+function paintResultShow(pResultShowSearch) {
   let liElement = document.createElement('li');
   liElement.setAttribute('class', 'page__main__conatiner__ul__li js-ul-movie');
   let titleElement = document.createElement('h2');
-  let textTitle = document.createTextNode(dataShowResult.name);
+  let textTitle = document.createTextNode(pResultShowSearch.name);
   let imgElement = document.createElement('img');
-  if (dataShowResult.image.medium !== '') {
-    imgElement.setAttribute('src', dataShowResult.image.medium);
+  if (pResultShowSearch.image.medium !== '') {
+    imgElement.setAttribute('src', pResultShowSearch.image.medium);
   } else {
     imgElement.setAttribute('src', 'https://via.placeholder.com/210x295/ffffff/666666/?text=TV');
   }
@@ -38,8 +41,15 @@ function paintResultShow() {
   liElement.appendChild(titleElement);
   liElement.appendChild(imgElement);
   ulElement.appendChild(liElement);
-  // eslint-disable-next-line no-console
-  console.log(dataShowResult);
+  liElement.addEventListener('click', handleFav);
 }
-
+function handleFav(ev) {
+  let contentTitleChildren = ev.currentTarget.firstElementChild.innerText;
+  let contentImgChildren = ev.currentTarget.lastElementChild.src;
+  showFavResult.push({
+    title: contentTitleChildren,
+    image: contentImgChildren
+  });
+  /* console.log(showFavResult); */
+}
 btnElement.addEventListener('click', getDataApi);
