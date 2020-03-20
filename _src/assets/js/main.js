ad34f574905755faa6e0555b205fdb29b14b63b2
Rosanna Blanco <rosanna.blanco.blanco@gmail.com>
2020-03-20 08:55:57 +0100

    Add proove with fetch

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,3 +1,17 @@
 'use strict';
+// global variables
+const inputElement = document.querySelector('.js-input');
+const btnElement = document.querySelector('.js-btn');
 
-console.log('>> Ready :)');
+fetch('http://api.tvmaze.com/search/shows?q=girls')
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+  });
+
+function getData() {
+  // eslint-disable-next-line no-console
+  console.log(inputElement.value);
+}
+
+btnElement.addEventListener('click', getData);
