bf7d112597dbe1bec3b95c31aa61031696130bba
Rosanna Blanco <rosanna.blanco.blanco@gmail.com>
2020-03-20 14:25:34 +0100

    paint list data

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -7,9 +7,6 @@ const ulElement = document.querySelector('.js-ul-movie');
 //arrays results
 let dataShowResult = new Array();
 
-//
-let dataShow = new Array();
-
 function getDataApi() {
   const inputElementValue = inputElement.value;
   fetch('http://api.tvmaze.com/search/shows?q=' + inputElementValue)
@@ -23,26 +20,28 @@ function getDataApi() {
 function saveDataShow(pData) {
   for (let i = 0; i < pData.length; i++) {
     let data = pData[i].show;
-    dataShow.push(data);
+    dataShowResult.push(data);
   }
   /*  console.log(dataShow); */
-  paintResultShow();
+  paintResultShow(dataShowResult);
 }
-function paintResultShow() {
-  let liElement = document.createElement('li');
-  liElement.setAttribute('class', 'page__main__conatiner__ul__li js-ul-movie');
-  let titleElement = document.createElement('h2');
-  let textTitle = document.createTextNode(dataShow.name);
-  let imgElement = document.createElement('img');
-  if (dataShow.image.medium !== '') {
-    imgElement.setAttribute('src', dataShow.image.medium);
-  } else {
-    imgElement.setAttribute('src', 'https://via.placeholder.com/210x295/ffffff/666666/?text=TV');
+function paintResultShow(pDataShowResult) {
+  for (const itemSerie of pDataShowResult) {
+    let liElement = document.createElement('li');
+    liElement.setAttribute('class', 'page__main__conatiner__ul__li js-ul-movie');
+    let titleElement = document.createElement('h2');
+    let textTitle = document.createTextNode(itemSerie.name);
+    let imgElement = document.createElement('img');
+    if (itemSerie.image.medium !== '') {
+      imgElement.setAttribute('src', itemSerie.image.medium);
+    } else {
+      imgElement.setAttribute('src', 'https://via.placeholder.com/210x295/ffffff/666666/?text=TV');
+    }
+    titleElement.appendChild(textTitle);
+    liElement.appendChild(titleElement);
+    liElement.appendChild(imgElement);
+    ulElement.appendChild(liElement);
   }
-  titleElement.appendChild(textTitle);
-  liElement.appendChild(titleElement);
-  liElement.appendChild(imgElement);
-  ulElement.appendChild(liElement);
 }
 
 btnElement.addEventListener('click', getDataApi);
