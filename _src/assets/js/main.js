de1e6e1ef198dbfec53bc17e37756fc54174627e
Rosanna Blanco <rosanna.blanco.blanco@gmail.com>
2020-03-21 09:00:41 +0100

    Init ulElement and array search

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,12 +1,11 @@
-<<<<<<< HEAD
 'use strict';
 // global variables
 const inputElement = document.querySelector('.js-input');
 const btnElement = document.querySelector('.js-btn');
 const ulElement = document.querySelector('.js-ul-movie');
 
-//arrays results
-let dataShowResult = new Array();
+//arrays results search serie
+let dataSerieResult = new Array();
 
 function getDataApi() {
   const inputElementValue = inputElement.value;
@@ -19,25 +18,22 @@ function getDataApi() {
 }
 
 function saveDataShow(pData) {
+  dataSerieResult = [];
   for (let i = 0; i < pData.length; i++) {
     let data = pData[i].show;
-    dataShowResult.push(data);
+    dataSerieResult.push(data);
   }
-  /*  console.log(dataShow); */
-  paintResultShow(dataShowResult);
+  paintResultShow(dataSerieResult);
 }
-function paintResultShow(pDataShowResult) {
-  for (const itemSerie of pDataShowResult) {
+function paintResultShow(pDataSerieResult) {
+  ulElement.innerHTML = '';
+  for (const itemSerie of pDataSerieResult) {
     let liElement = document.createElement('li');
     liElement.setAttribute('class', 'page__main__conatiner__ul__li js-ul-movie');
     let titleElement = document.createElement('h2');
     let textTitle = document.createTextNode(itemSerie.name);
     let imgElement = document.createElement('img');
-    if (itemSerie.image.medium !== '') {
-      imgElement.setAttribute('src', itemSerie.image.medium);
-    } else {
-      imgElement.setAttribute('src', 'https://via.placeholder.com/210x295/ffffff/666666/?text=TV');
-    }
+    imgElement.setAttribute('src', itemSerie.image.medium);
     titleElement.appendChild(textTitle);
     liElement.appendChild(titleElement);
     liElement.appendChild(imgElement);
@@ -46,33 +42,3 @@ function paintResultShow(pDataShowResult) {
 }
 
 btnElement.addEventListener('click', getDataApi);
-=======
-'use strict';
-// global variables
-const inputElement = document.querySelector('.js-input');
-const btnElement = document.querySelector('.js-btn');
-
-//
-let dataShow = new Array();
-
-function getDataApi() {
-  const inputElementValue = inputElement.value;
-  fetch('http://api.tvmaze.com/search/shows?q=' + inputElementValue)
-    .then(response => response.json())
-    .then(data => {
-      saveDataShow(data);
-    });
-  // eslint-disable-next-line no-console
-}
-
-//function save result data API
-function saveDataShow(pData) {
-  for (let i = 0; i < pData.length; i++) {
-    let data = pData[i].show;
-    dataShow.push(data);
-  }
-  /*  console.log(dataShow); */
-}
-
-btnElement.addEventListener('click', getDataApi);
->>>>>>> fetch
